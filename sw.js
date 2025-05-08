008ed36b2038d53d8c2087aba1982a4114b5d65f
destructo321 <pngengkeat@gmail.com>
2025-05-08 18:23:53 +0800

    change to PWA

---
+++ sw.js
@@ -0,0 +1,59 @@
+// Service Worker script (sw.js)
+
+// Define the cache name
+const CACHE_NAME = 'scratchpad-cache-v1';
+
+// List the files to cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+  // Add paths to your icon files here, e.g.:
+  // '/icons/icon-192x192.png',
+  // '/icons/icon-512x512.png'
+];
+
+// Install event: Cache static assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event: Serve cached assets when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // No cache hit - fetch from network
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate event: Clean up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            // Delete old caches
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
